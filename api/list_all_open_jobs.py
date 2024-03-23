ab61c0127723140334b63b6e0687666950683db7
dustein <steindu@gmail.com>
2024-03-22 21:59:09 -0300

    pensar em filtros

--- api/list_all_open_jobs.py
+++ api/list_all_open_jobs.py
@@ -11,6 +11,7 @@ def lambda_handler(event, context):
 
   response = []
 
+
   for num in range (1,5):
     equipe = f'EQUIPE#{num}'
     
@@ -18,9 +19,25 @@ def lambda_handler(event, context):
       IndexName = 'equipe-accept-index',
       KeyConditionExpression = Key ('GSI1-PK').eq(equipe) & Key ('GSI1-SK').eq('USER_ACCEPTED#none')
       )
+# !! Criar regra para caso seja informado a equipe do operador, não exibir os jobs da mesma
+    
+# !! Criar regra para exibir somente full/day/nigth/todos
+# !! Criar regra para exibir jobs de uma equipe específica apenas
+#se for indicado uma equipe
+    match equipe_job:
+      case equipe_job['Count'] > 0:
+        
 
-    if equipe_job['Count'] > 0:
-      response.append(equipe_job['Items'])
+    if event['queryStringParameters']['GSI1-PK']:
+      #nao incluir se for a mesma equipe do usuario
+      if event['queryStringParameters'] != equipe:
+        if equipe_job['Count'] > 0:
+          response.append(equipe_job['Items'])
+
+
+
+
+
 
   if response:
     return response_builder(200, response)
